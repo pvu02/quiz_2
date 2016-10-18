bba98cf698bf4d0e127ecfb88b9073435568cf41
Janie Vu <Janie Vu>
2016-10-18 13:09:11 -0400

    Third commit (1:09pm)

--- quiz_script.R
+++ quiz_script.R
@@ -5,17 +5,15 @@ library(apaTables)
 library(dplyr)
 bfi_data <- psych::bfi
 
-categorical_variables <- select(bfi_data, gender, education)
+categorical_variables <- select(bfi_data, gender)
 categorical_variables$gender <- as.factor(categorical_variables$gender)
-categorical_variables$education <- as.factor(categorical_variables$education)
 levels(categorical_variables$gender) <- list("Male"=1, "Female"=2)
-levels(categorical_variables$education) <- list("HS"=1,"finished HS"=2,"some college"=3,"college graduate"=4,"graduate degree"=5)
-
 
 agreeableness <- select (bfi_data, A1,A2,A3,A4,A5)
 extraversion <- select (bfi_data, E1,E2,E3,E4,E5)
 neuroticism <- select (bfi_data, N1,N2,N3,N4,N5)
 age <- select (bfi_data, age)
+education <- select (bfi_data, education)
 
 is_bad_value <- agreeableness<1 | agreeableness>6
 agreeableness[is_bad_value] <- NA
@@ -35,6 +33,16 @@ psych::describe(neuroticism)
 agreeableness <- psych::alpha(as.data.frame(agreeableness),check.keys=FALSE)$scores
 extraversion <- psych::alpha(as.data.frame(extraversion),check.keys=FALSE)$scores
 neuroticism <- psych::alpha(as.data.frame(neuroticism),check.keys=FALSE)$scores
-analytic_data <- cbind(categorical_variables,age,agreeableness,extraversion,neuroticism)
+analytic_data <- cbind(categorical_variables,education,age,agreeableness,extraversion,neuroticism)
 
 write_csv(analytic_data,path="analytic_data.csv")
+
+apa.cor.table(analytic_data, filename="Table1.doc", table.number=1)
+
+analytic_data_male <- filter (analytic_data, gender=='Male')
+analytic_data_male <- select(analytic_data_male, -gender)
+analytic_data_male <- filter (analytic_data_male, age>40)
+
+write_csv(analytic_data_male,path="analytic_data_male.csv")
+
+apa.cor.table(analytic_data_male, filename="Table2.doc", table.number=2)
